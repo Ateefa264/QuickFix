07bccfdca72df60ce0d66de17bba562e84d3e59b
Mariam-04 <mararif2004@gmail.com>
2025-04-29 22:19:46 +0500

    Update requestmodel.js

--- requestmodel.js
+++ requestmodel.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 const requestSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     professional: { type: mongoose.Schema.Types.ObjectId, ref: 'Professional' },
-    service: { type: mongoose.Schema.Types.ObjectId, ref: 'Service' },
+    service: { type: String },  
     status: { type: String, default: 'pending' },
     scheduledTime: Date,
     location: String,
     details: String
-  }, { timestamps: true });
-  
-  module.exports = mongoose.model('Request', requestSchema);
+}, { timestamps: true });
+
+module.exports = mongoose.model('Request', requestSchema);
