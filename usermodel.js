03427a5e971c28399a1e26c3881fa33c0da66fb3
Mariam-04 <mararif2004@gmail.com>
2025-04-29 22:18:01 +0500

    Update usermodel.js

--- usermodel.js
+++ usermodel.js
@@ -7,9 +7,28 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
   phone: { type: String, required: true },
   address: { type: String, required: false },
-  user_type: { type: String, enum: ['Customer', 'Professional'], required: true },
+  user_type: { type: String, enum: ['user', 'Professional'], required: true },
   profile_picture_url: { type: String },
   created_at: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('User', userSchema);
+
+
+/*const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  phone: { type: String, required: true },
+  address: { type: String, required: true },
+  user_type: {
+    type: String,
+    enum: ['user', 'professional'], // This limits user_type to only 'user' or 'professional'
+    required: true
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema);
+*/
